aac29b56cad6c2a7f36630b577421e979a265490
cdbm2526 <calebtoussaint@gmail.com>
2026-07-23 15:08:37 -0400

    Endpoint retournant un tableau JSON avec le parametre

--- src/main/java/ht/mbds/calebtoussaint/tp3guidetouristiquecalebtoussaint/resource/GuideTouristiqueResource.java
+++ src/main/java/ht/mbds/calebtoussaint/tp3guidetouristiquecalebtoussaint/resource/GuideTouristiqueResource.java
@@ -17,12 +17,12 @@ public class GuideTouristiqueResource {
      * Retourne des informations touristiques pour la ville ou le pays donné.
      *
      * @param villeOuPays le nom de la ville ou du pays sur lequel on veut des informations
-     * @return pour le moment, simplement le paramètre reçu
+     * @return un tableau JSON contenant le nom du lieu (pour test)
      */
     @GET
     @Path("lieu/{ville_ou_pays}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String endroitsDeVisite(@PathParam("ville_ou_pays") String villeOuPays) {
-        return villeOuPays;
+    @Produces(MediaType.APPLICATION_JSON)
+    public String[] endroitsDeVisite(@PathParam("ville_ou_pays") String villeOuPays) {
+        return new String[] { villeOuPays };
     }
 }
